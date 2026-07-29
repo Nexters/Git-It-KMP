126913bfa869b15c903d9607e2bc800fb1123fb1
Hoseong-Ryu <hoseong091234@gmail.com>
2026-07-30 06:07:37 +0900

    refactor: 플랫폼 앱 composition root에서 dataModule 조립
    dataModule(baseUrl) 등록을 각 플랫폼 앱으로 이동한다.
    baseUrlQualifier named qualifier를 제거하고 BuildConfig/AuthConfig에서 직접 전달한다.

--- androidApp/src/main/kotlin/com/nexters/hytime/gitit/GitItApplication.kt
+++ androidApp/src/main/kotlin/com/nexters/hytime/gitit/GitItApplication.kt
@@ -3,13 +3,13 @@ package com.nexters.hytime.gitit
 import android.content.Context
 import android.app.Application
 import com.nexters.hytime.gitit.BuildConfig
-import com.nexters.hytime.gitit.logging.initLogger
 import com.nexters.hytime.gitit.auth.AndroidGoogleAuthenticatorFactory
 import com.nexters.hytime.gitit.auth.GoogleAuthenticatorFactory
-import com.nexters.hytime.gitit.baseUrlQualifier
+import com.nexters.hytime.gitit.data.di.dataModule
+import com.nexters.hytime.gitit.logging.initLogger
 import org.koin.android.ext.koin.androidContext
-import org.koin.dsl.module
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class GitItApplication : Application() {
     override fun onCreate() {
@@ -17,7 +17,7 @@ class GitItApplication : Application() {
         initLogger(BuildConfig.DEBUG)
         startKoin {
             androidContext(this@GitItApplication)
-            modules(appModules + platformModule)
+            modules(appModules + dataModule(BuildConfig.BACKEND_BASE_URL) + platformModule)
         }
     }
 }
@@ -25,7 +25,7 @@ class GitItApplication : Application() {
 /**
  * Android 플랫폼 전용 DI 바인딩이다.
  *
- * Credential Manager에 필요한 [android.content.Context]와 백엔드 검증용
+ * Credential Manager에 필요한 [Context]와 백엔드 검증용
  * Web Client ID로 [AndroidGoogleAuthenticatorFactory]를 생성한다.
  */
 private val platformModule =
@@ -36,5 +36,4 @@ private val platformModule =
                 serverClientId = BuildConfig.GOOGLE_WEB_CLIENT_ID,
             )
         }
-        single(baseUrlQualifier) { BuildConfig.BACKEND_BASE_URL }
     }
--- desktopApp/src/main/kotlin/com/nexters/hytime/gitit/Main.kt
+++ desktopApp/src/main/kotlin/com/nexters/hytime/gitit/Main.kt
@@ -4,14 +4,20 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.nexters.hytime.gitit.auth.DesktopGoogleAuthenticatorFactory
 import com.nexters.hytime.gitit.auth.GoogleAuthenticatorFactory
+import com.nexters.hytime.gitit.data.di.dataModule
 import com.nexters.hytime.gitit.logging.initLogger
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
+/**
+ * 데스크톱 애플리케이션 진입점이다.
+ *
+ * Kermit 로거를 초기화하고 Koin DI를 시작한 뒤, Compose Desktop 윈도우를 띄운다.
+ */
 fun main() {
     initLogger(isDebug = true)
     startKoin {
-        modules(appModules + platformModule)
+        modules(appModules + dataModule(AuthConfig.BACKEND_BASE_URL) + platformModule)
     }
 
     application {
@@ -38,5 +44,4 @@ private val platformModule =
                 clientSecret = AuthConfig.GOOGLE_DESKTOP_CLIENT_SECRET,
             )
         }
-        single(baseUrlQualifier) { AuthConfig.BACKEND_BASE_URL }
     }
